5cbe4f484e17b3803ce1b8480472cf8473696f1d
unknown <rahulparadkar08>
2024-07-10 18:32:15 +0530

    protecting routes with guards

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -8,11 +8,18 @@ import { User } from 'src/users/entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import jwtConfig from './config/jwt.config';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from './authentication/guards/access-token/access-token.guard';
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), JwtModule.registerAsync(jwtConfig.asProvider()), ConfigModule.forFeature(jwtConfig)],
-  providers: [{ provide: HashingService, useClass: BcryptService }, AuthenticationService],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig)],
+    providers: [{ provide: HashingService, useClass: BcryptService },
+      {provide: APP_GUARD,useClass: AccessTokenGuard  },
+      AuthenticationService],
   controllers: [AuthenticationController]
 })
 export class IamModule { }
